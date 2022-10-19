7d015b91802a3ada7a2ad33fb4f3e5b08e7f9e4b
dfgion <113937888+dfgion@users.noreply.github.com>
2022-10-19 23:53:00 +0300

    update description about method

--- date.py
+++ date.py
@@ -17,7 +17,7 @@ class Date_and_time:
         date[1] = date[1].split(':') # Процесс, описанный выше
         date[1].remove(date[1][2]) # Процесс, описанный выше
         date = date[0] + date[1] # Преобразовываем измененные элементы в 1 список
-        date = {'year': int(date[0]), 'month' : int(date[1]), 'day' : int(date[2]), 'hour' : int(date[3]), 'minute' : int(date[4]) } # Так как библиотека datetime принимает в своем методе datetime только целые значения, я создаю словарь
+        date = {'year': int(date[0]), 'month' : int(date[1]), 'day' : int(date[2]), 'hour' : int(date[3]), 'minute' : int(date[4]) } # Так как библиотека datetime принимает в своем методе datetime только целые значения, а не список, я создаю словарь, значениями для которого будут элементы списка. Такии образом я распаковываю все значения из списка
         date = datetime.datetime(date['year'], date['month'], date['day'], date['hour'], date['minute'])
         result=time.mktime(date.timetuple()) #Преобразовываем в Unix, не вызывал функцию, так как она принимает только 1 аргумент, а мне бы пришлось передовать словарь
         return result
